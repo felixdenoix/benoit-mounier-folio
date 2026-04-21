ef09586c685b5883e0b621457f53c29b65ac791f
Felix Lafeuille <felix.denoix@gmail.com>
2026-04-21 19:45:18 +0200

    feature: home - experience done

--- site/snippets/home/expertise.php
+++ site/snippets/home/expertise.php
@@ -1,9 +1,10 @@
-<div class="h-[50vh] pt-2 p-4 ">
+<div class="h-screen">
 </div>
+
 
 <c-home-expertise
     log
-    class="wrapper relative w-11/12 md:w-10/12 lg:w-9/12 xl:w-8/12 min-h-screen text-white">
+    class="wrapper relative w-11/12 md:w-10/12 lg:w-9/12 xl:w-8/12 min-h-[min(100vh,900px)] text-white">
 
     <div
         class=" text-gray-text-light text-2xl w-full text-center gap-grid-padding mb-16 block expertise-title font-bold"
@@ -15,44 +16,44 @@
     </div>
 
     <div
-        class="animate-opacity flex flex-col md:flex-row gap-y-8 md:gap-unset justify-between items-center w-full mb-16 text-white group py-[15vh] md:py-0 md:pb-[calc(40px+33vh)] relative bdr z-0"
+        class="animate-opacity flex flex-col md:flex-row justify-between items-center w-full mb-16 text-white group py-[10vh] md:py-0 md:pb-[calc(40px+33vh)] relative z-0"
         string
         string-offset-bottom="-30%">
 
         <div
-            class="bdg pointer-events-none sticky md:absolute top-1/2 md:top-[unset] md:bottom-0 left-0 w-full h-0 md:h-[33vh] z-0"
+            class="pointer-events-none sticky md:absolute top-1/2 md:top-[unset] md:bottom-0 left-0 w-full h-0 md:h-[33vh] z-0"
             data-dom="media-display">
             <?php foreach ($ctaItems as $index => $item): ?>
                 <div
                     class="absolute inset-0 h-[33vh] -translate-y-1/2 md:translate-y-0 grid grid-cols-1 grid-rows-1 place-items-stretch opacity-0 scale-99 transition duration-300 ease-projects"
                     data-dom="media-group"
                     data-expertise-id="<?= $item["id"] ?>">
-                        <?php foreach ($item["projects"] as $link_project): ?>
-                            <?php snippet("imagex-picture", [
-                                "image" => $link_project["asset"],
-                                "attributes" => [
-                                    "picture" => [
-                                        "class" => "col-[1/1] row-[1/1] place-self-stretch relative",
-                                    ],
-                                    "img" => [
-                                        "shared" => [
-                                            "class" =>
-                                                "w-full h-full object-contain object-center scale-101 scale-99! transition duration-400 ease-projects " .
-                                                ($item["projects_count"] > 1 ? "opacity-0" : "opacity-100"),
-                                            "sizes" => "600px",
-                                        ],
+                    <?php foreach ($item["projects"] as $link_project): ?>
+                        <?php snippet("imagex-picture", [
+                            "image" => $link_project["asset"],
+                            "attributes" => [
+                                "picture" => [
+                                    "class" => "col-[1/1] row-[1/1] place-self-stretch relative",
+                                ],
+                                "img" => [
+                                    "shared" => [
+                                        "class" =>
+                                            "w-full h-full object-contain object-center scale-101 transition duration-400 brightness-80 ease-projects " .
+                                            ($item["projects_count"] > 1 ? "opacity-0 scale-99!" : "opacity-100"),
+                                        "sizes" => "600px",
                                     ],
                                 ],
-                                "srcset" => "ben-srcset",
-                            ]); ?>
-                        <?php endforeach; ?>
+                            ],
+                            "srcset" => "ben-srcset",
+                        ]); ?>
+                    <?php endforeach; ?>
                 </div>
             <?php endforeach; ?>
         </div>
 
         <?php foreach ($ctaItems as $index => $item): ?>
             <a
-                class="bdb expertise-el z-10 cap block uppercase text-[1.3rem] font-bold py-3 md:py-0 transition opacity-0 text-shadow-2xs md:text-shadow-none group-[&.-inview]:opacity-100 group-[&.-inview]:delay-[calc(var(--expertise-index)*55ms+60ms)] ease-projects duration-500 not-pointer-coarse:group-has-[:hover]:text-gray-text-light not-pointer-coarse:hover:text-white group/link"
+                class="expertise-el z-10 cap block uppercase text-[1.3rem] font-bold py-8 md:py-0 transition opacity-0 text-shadow-sm text-gray-text-light md:text-white md:text-shadow-none group-[&.-inview]:opacity-100 group-[&.-inview]:delay-[calc(var(--expertise-index)*55ms+60ms)] ease-projects duration:300 md:duration-500 not-pointer-coarse:group-has-[:hover]:text-gray-text-light not-pointer-coarse:hover:text-white group/link"
                 style="--expertise-index:<?= $index ?>;"
                 href="<?= $item["url"] ?>"
                 data-dom="expertise"
@@ -60,10 +61,36 @@
                 data-expertise-projects="<?= esc($item["projects_encoded"], "html") ?>"
                 data-projects-count="<?= $item["projects_count"] ?>">
                 <span class="pointer-events-none"><?= $item["label"] ?></span>
+                <div
+                    class="hidden pointer-events-none absolute bottom-0 left-0 w-full h-[33vh] md:grid grid-cols-1 grid-rows-1 place-items-stretch min-w-0 min-h-0 opacity-0 scale-99 transition duration-500 ease-projects group-hover/link:opacity-100 group-hover/link:scale-101"
+                    data-dom="desktop-media-group"
+                    data-expertise-id="<?= $item["id"] ?>">
+                    <?php foreach ($item["projects"] as $index => $link_project): ?>
+                        <?php snippet("imagex-picture", [
+                            "image" => $link_project["asset"],
+                            "attributes" => [
+                                "picture" => [
+                                    "class" => "col-[1/1] row-[1/1] place-self-stretch relative",
+                                ],
+                                "img" => [
+                                    "shared" => [
+                                        "class" =>
+                                            "w-full h-full object-contain object-center " .
+                                            ($item["projects_count"] > 1
+                                                ? " scale-101 opacity-0 scale-99! transition duration-500 ease-projects"
+                                                : " opacity-100"), // TODO: handle opacity based on index
+                                        "sizes" => "600px",
+                                    ],
+                                ],
+                            ],
+                            "srcset" => "ben-srcset",
+                        ]); ?>
+                    <?php endforeach; ?>
+                </div>
             </a>
             <hr
                 aria-hidden
-                class="w-6 md:hidden block m-3 last-of-type:hidden">
+                class="w-6 md:hidden block m-3 last-of-type:hidden opacity-0 group-[&.-inview]:opacity-100 duration-500 ease-projects delay-500">
             <span
                 aria-hidden
                 class="hidden md:block last:hidden opacity-0 group-[&.-inview]:opacity-100 duration-500 ease-projects delay-500">|</span>
--- src/components/home-expertise.ts
+++ src/components/home-expertise.ts
@@ -7,8 +7,10 @@ interface ExpertiseData {
   el: HTMLAnchorElement;
   projects: any[];
   projectsCount: number;
-  mediaGroup: HTMLElement | null;
-  assets: HTMLElement[];
+  mobileMediaGroup: HTMLElement | null;
+  mobileAssets: HTMLElement[];
+  desktopMediaGroup: HTMLElement | null;
+  desktopAssets: HTMLElement[];
   activeAssetIndex?: number;
   bcr?: DOMRect;
 }
@@ -47,8 +49,11 @@ export default class HomeExpertise extends Piece {
   private init() {
     this.$expertiseEls = Array.from((this.domAttr("expertise") as NodeList) || []) as HTMLAnchorElement[];
 
-    const mediaGroups = Array.from(this.domAttr("media-group") as NodeList) as HTMLElement[];
-    const mediaMap = new Map(mediaGroups.map((mg) => [mg.dataset.expertiseId, mg]));
+    const mobileGroups = Array.from((this.domAttr("media-group") as NodeList) || []) as HTMLElement[];
+    const desktopGroups = Array.from((this.domAttr("desktop-media-group") as NodeList) || []) as HTMLElement[];
+
+    const mobileMap = new Map(mobileGroups.map((mg) => [mg.dataset.expertiseId, mg]));
+    const desktopMap = new Map(desktopGroups.map((dg) => [dg.dataset.expertiseId, dg]));
 
     this.$expertiseEls.forEach((el) => {
       const id = el.dataset.expertiseId;
@@ -57,15 +62,21 @@ export default class HomeExpertise extends Piece {
       const projectsRaw = el.dataset.expertiseProjects;
       const projects = projectsRaw ? JSON.parse(projectsRaw) : [];
       const projectsCount = el.dataset.projectsCount ? parseInt(el.dataset.projectsCount) : 0;
-      const mediaGroup = mediaMap.get(id) as HTMLElement;
-      const assets = mediaGroup ? Array.from(mediaGroup.querySelectorAll("img")) : [];
+
+      const mobileMediaGroup = mobileMap.get(id) || null;
+      const mobileAssets = mobileMediaGroup ? Array.from(mobileMediaGroup.querySelectorAll("img")) : [];
+
+      const desktopMediaGroup = desktopMap.get(id) || null;
+      const desktopAssets = desktopMediaGroup ? Array.from(desktopMediaGroup.querySelectorAll("img")) : [];
 
       this.expertises.set(id, {
         el,
         projects,
         projectsCount,
-        mediaGroup,
-        assets,
+        mobileMediaGroup,
+        mobileAssets,
+        desktopMediaGroup,
+        desktopAssets,
       });
     });
   }
@@ -73,23 +84,39 @@ export default class HomeExpertise extends Piece {
   private setup() {
     this.previousIsDesktop = this.isDesktop;
     this.isDesktop = window.matchMedia("(min-width: 768px)").matches;
-    this.observer?.disconnect();
+
+    const modeChanged = this.isDesktop !== this.previousIsDesktop;
+
+    if (this.observer) {
+      this.observer.disconnect();
+      this.observer = undefined;
+    }
 
     if (!this.isDesktop) {
       this.setupObserver();
     }
 
-    this.expertises.forEach((data) => {
-      if (this.previousIsDesktop) {
+    this.expertises.forEach((data, id) => {
+      const elProjectsCount = data.projectsCount;
+
+      if (elProjectsCount > 1 && this.previousIsDesktop && !this.isDesktop) {
         data.el.removeEventListener("mousemove", this.onMouseMove);
         data.el.removeEventListener("mouseleave", this.onMouseLeave);
       }
+
       if (this.isDesktop) {
-        data.el.addEventListener("mousemove", this.onMouseMove);
-        data.el.addEventListener("mouseleave", this.onMouseLeave);
-        data.bcr = data.el.getBoundingClientRect();
+        if (elProjectsCount > 1) {
+          data.el.addEventListener("mousemove", this.onMouseMove);
+          data.el.addEventListener("mouseleave", this.onMouseLeave);
+          data.bcr = data.el.getBoundingClientRect();
+        }
       } else {
         this.observer?.observe(data.el);
+      }
+
+      // If we switched between desktop/mobile, force an update of the new active set
+      if (modeChanged && data.activeAssetIndex !== undefined) {
+        this.updateAsset(id, data.activeAssetIndex, true);
       }
     });
   }
@@ -100,27 +127,33 @@ export default class HomeExpertise extends Piece {
     // Estimate margin based on first element
     const firstExp = this.expertises.values().next().value;
     const ctaHeight = firstExp?.el.getBoundingClientRect().height || 50;
-    const viewportHeight = globalThis.app.consts.innerHeight;
-    const margin = Math.round((viewportHeight - ctaHeight * 2) / 2);
+    const viewportHeight = window.innerHeight;
+    const margin = Math.round((viewportHeight - ctaHeight) / 2);
 
     this.observer = new IntersectionObserver(
       (entries) => {
         if (this.isDesktop) return;
-
         entries.forEach((entry) => {
           const id = (entry.target as HTMLElement).dataset.expertiseId;
           if (!id) return;
 
           if (entry.intersectionRatio > 0.6) {
-            const progress = getIntersectionProgress(entry);
-            this.setActiveExpertise(id, progress);
+            const data = this.expertises.get(id);
+            if (data) {
+              if (data.projectsCount > 1) {
+                const progress = getIntersectionProgress(entry);
+                this.setActiveExpertise(id, progress);
+              } else if (this.activeId !== id) {
+                this.setActiveExpertise(id, 0);
+              }
+            }
           } else if (this.activeId === id) {
             this.setActiveExpertise(null);
           }
         });
       },
       {
-        threshold: createThresholdArray(30),
+        threshold: createThresholdArray(15),
         rootMargin: `-${margin}px 0px -${margin}px 0px`,
       },
     );
@@ -147,15 +180,13 @@ export default class HomeExpertise extends Piece {
   };
 
   private setActiveExpertise(id: string | null, progress: number = 0) {
-    console.log("😸 setActiveExpertise");
     if (this.activeId !== id) {
-      if (this.activeId) {
-        this.toggleExpertise(this.activeId, false);
+      // Only toggle visibility via JS on mobile
+      if (!this.isDesktop) {
+        if (this.activeId) this.toggleExpertise(this.activeId, false);
+        if (id) this.toggleExpertise(id, true);
       }
       this.activeId = id;
-      if (id) {
-        this.toggleExpertise(id, true);
-      }
     }
 
     if (id) {
@@ -168,44 +199,48 @@ export default class HomeExpertise extends Piece {
   }
 
   private toggleExpertise(id: string, active: boolean) {
-    console.log("😸 toggleExpertise");
     const data = this.expertises.get(id);
-    if (!data || !data.mediaGroup) return;
+    if (!data) return;
 
     frameDOM.mutate(() => {
-      data.mediaGroup?.classList.toggle("opacity-100", active);
-      data.mediaGroup?.classList.toggle("opacity-0", !active);
-      data.mediaGroup?.classList.toggle("scale-101", active);
-      data.mediaGroup?.classList.toggle("scale-99", !active);
+      data.el.classList.toggle("text-white", active);
 
-      // Default to first asset if becoming active
-      if (active && data.activeAssetIndex === undefined) {
+      if (!this.isDesktop && data.mobileMediaGroup) {
+        data.mobileMediaGroup.classList.toggle("opacity-100", active);
+        data.mobileMediaGroup.classList.toggle("opacity-0", !active);
+        data.mobileMediaGroup.classList.toggle("scale-101", active);
+        data.mobileMediaGroup.classList.toggle("scale-99", !active);
+      }
+
+      // Default to first asset if becoming active and there are multiple projects
+      if (active && data.activeAssetIndex === undefined && data.projectsCount > 1) {
         this.updateAsset(id, 0);
       }
     });
   }
 
-  private updateAsset(id: string, index: number) {
-    console.log("😸 updateAsset");
+  private updateAsset(id: string, index: number, force: boolean = false) {
     const data = this.expertises.get(id);
-    if (!data || data.activeAssetIndex === index) return;
+    if (!data || (!force && data.activeAssetIndex === index)) return;
 
     frameDOM.mutate(() => {
-      if (data.activeAssetIndex !== undefined) {
-        const prevAsset = data.assets[data.activeAssetIndex];
-        prevAsset?.classList.add("opacity-0", "scale-99!");
-      }
+      const assets = this.isDesktop ? data.desktopAssets : data.mobileAssets;
 
-      const nextProjectAsset = data.assets[index];
-      if (nextProjectAsset) {
-        nextProjectAsset.classList.remove("opacity-0", "scale-99!");
-        data.activeAssetIndex = index;
+      assets.forEach((asset, i) => {
+        const isActive = i === index;
+        asset.classList.toggle("opacity-0", !isActive);
+        asset.classList.toggle("scale-99!", !isActive);
+        // We don't necessarily need to add opacity-100 if the PHP starts with it or other classes,
+        // but for safety and to match the 'hide' logic:
+        if (isActive) asset.classList.remove("opacity-0", "scale-99!");
+      });
 
-        // Update URL on desktop
-        const nextProjectUrl = data.projects[index]?.url;
-        if (nextProjectUrl && data.el.href !== nextProjectUrl) {
-          data.el.href = data.projects[index].url;
-        }
+      data.activeAssetIndex = index;
+
+      // Update URL on desktop
+      const nextProjectUrl = data.projects[index]?.url;
+      if (nextProjectUrl && data.el.href !== nextProjectUrl) {
+        data.el.href = nextProjectUrl;
       }
     });
   }
