c0d6bc8bd758c4426c59a5c2ad583e21aef1f0c5
Felix Lafeuille <felix.denoix@gmail.com>
2026-04-29 14:02:39 +0200

    tune: progress loader position on moible

--- site/snippets/site-loader.php
+++ site/snippets/site-loader.php
@@ -1,6 +1,6 @@
-<div id="site-loader" style="position:fixed; top:0; left:0; height:100vh; width:100vw; z-index:1000; background-color:white; font-variation-settings: 'onum';">
+<div id="site-loader" style="position:fixed; top:0; left:0; height:100dvh; width:100vw; z-index:1000; background-color:white; font-variation-settings: 'onum';">
 
-    <div style="position:absolute; bottom:0; right:0; padding:36px; line-height:12px;" class="text-cap">
+    <div style="position:absolute; bottom:0; right:0; padding:36px 36px calc(36px + env(safe-area-inset-bottom, 0px)); line-height:12px;" class="text-cap">
         <span data-dom="progress">0</span>/<span>100</span>
     </div>
 
